08ed5220f14374dc1ff39e5a3704fd9af24e4597
Teofilo Betancourt <tbetancourt@theatgroup.net>
2024-01-24 17:37:33 -0400

    Initial commit

--- app/src/main/java/com/appreman/app/Adapter/ElementoAdapter.java
+++ app/src/main/java/com/appreman/app/Adapter/ElementoAdapter.java
@@ -5,13 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.appreman.app.Database.DBHelper;
 import com.appreman.app.Models.Elemento;
 import com.appreman.app.Models.Pregunta;
 import com.appreman.appreman.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.List;
 
 public class ElementoAdapter extends RecyclerView.Adapter<ElementoAdapter.MotivosViewHolder> {
@@ -51,6 +56,15 @@ public class ElementoAdapter extends RecyclerView.Adapter<ElementoAdapter.Motivo
 
         PreguntaAdapter adapter = new PreguntaAdapter(mPreguntas);
         holder.recycler.setAdapter(adapter);
+
+        // Agregar funcionalidad al botón dentro de onBindViewHolder
+        holder.ftnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Mostrar un Toast como prueba
+                Toast.makeText(context, "Datos guardados correctamente", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -61,11 +75,13 @@ public class ElementoAdapter extends RecyclerView.Adapter<ElementoAdapter.Motivo
     protected static class MotivosViewHolder extends RecyclerView.ViewHolder {
         private final TextView txtElemento;
         private final RecyclerView recycler;
+        private final FloatingActionButton ftnGuardar;  // Cambia el tipo a FloatingActionButton
 
         public MotivosViewHolder(@NonNull View itemView) {
             super(itemView);
             txtElemento = itemView.findViewById(R.id.textNombre);
             recycler = itemView.findViewById(R.id.recyclerview);
+            ftnGuardar = itemView.findViewById(R.id.ftnGuardar);  // Asegúrate de que el ID sea el correcto
         }
     }
 }
